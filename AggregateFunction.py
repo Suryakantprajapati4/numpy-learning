339de4e8e8b587105212d14759ee06064a8a2563
Suryakant Prajapati <suryakantprajapati480@gmail.com>
2026-03-18 18:49:35 +0530

    Add files via upload

---
+++ AggregateFunction.py
@@ -0,0 +1,15 @@
+#aggregate Fuction = summarize data and typically return a single value
+
+import numpy as np
+array= np.array([[1,2,3,4],
+                 [5,6,7,8]])
+
+#print(np.sum(array))
+#print(np.mean(array))
+#print(np.std(array))
+#print(np.var(array))
+#print(np.min(array))
+#print(np.max(array))
+#print(np.argmin(array))
+#print(np.argmax(array))
+
